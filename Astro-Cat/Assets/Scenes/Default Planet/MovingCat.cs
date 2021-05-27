176b27d163c078b186ffbbd81a0ce738650eb900
yangyujin <asxpyn@naver.com>
2021-05-27 15:09:14 +0900

    Default Planet Cat and slope

--- Astro-Cat/Assets/Scenes/Default Planet/MovingCat.cs
+++ Astro-Cat/Assets/Scenes/Default Planet/MovingCat.cs
@@ -19,22 +19,22 @@ public class MovingCat : MonoBehaviour
         anim = GetComponent<Animator>();
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            //anim.Play("Walking");
+
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //anim.Play("Walking");
+
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            //anim.Play("Walking");
+
             transform.position += Vector3.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            //anim.Play("Walking");
+
             transform.position += Vector3.back * speed * Time.deltaTime;
         }
     }
