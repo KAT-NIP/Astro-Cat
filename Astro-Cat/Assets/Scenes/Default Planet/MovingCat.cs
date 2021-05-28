fe1c0b85052d413dca1aeb9232d0e9ba584f4e14
yangyujin <asxpyn@naver.com>
2021-05-28 15:52:09 +0900

    Add Jump on Default Planet

--- Astro-Cat/Assets/Scenes/Default Planet/MovingCat.cs
+++ Astro-Cat/Assets/Scenes/Default Planet/MovingCat.cs
@@ -7,32 +7,69 @@ public class MovingCat : MonoBehaviour
     public float speed = 2.0f;
     float hAxis;
     float vAxis;
+    bool jDown;
+
+    bool isJump = false;
 
     Vector3 moveVec;
+
+    Rigidbody rigid;
     Animator anim;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
-    // Start is called before the first frame update
-    void Start()
+
+
+    void Update()
     {
-        
+        GetInput();
+        Move();
+        Turn();
+        Jump();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        jDown = Input.GetButton("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * Time.deltaTime;
 
         anim.SetBool("isWalk", moveVec != Vector3.zero);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 6, ForceMode.Impulse);
+            anim.SetTrigger("doJump");
+            isJump = true;
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+
+            isJump = false;
+
+        }
     }
 }
