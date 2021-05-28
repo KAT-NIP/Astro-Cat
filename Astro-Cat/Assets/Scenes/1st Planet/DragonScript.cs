7694a368febceb066157186e13fb3fd9416bc748
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-05-28 20:02:43 +0900

    Add Dragon
    드래곤 집어넣긴 넣었는데 함수도 넣어줬는데 안나와요ㅠ

--- Astro-Cat/Assets/Scenes/1st Planet/DragonScript.cs
+++ Astro-Cat/Assets/Scenes/1st Planet/DragonScript.cs
@@ -1,26 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneMovement1st : MonoBehaviour
+public class DragonScript : MonoBehaviour
 {
     public Player player;
+
     // Start is called before the first frame update
     void Start()
-    {    }
+    {
+        gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.onFloor)
+        if(player.onFloor)
         {
-            Invoke("SceneMove", 10.0f);
+            gameObject.SetActive(true);
         }
-    }
-
-    public void SceneMove()
-    {
-        SceneManager.LoadScene("2048Scene");
     }
 }
