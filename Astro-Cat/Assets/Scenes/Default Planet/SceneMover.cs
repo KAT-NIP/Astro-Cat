03e3e2a95246777c202a941bafc5c9b65fa2b8cc
n-y-kim <nyn2265@gmail.com>
2021-05-28 19:28:21 +0900

    added scene change(fadeout 주석)

--- Astro-Cat/Assets/Scenes/Default Planet/SceneMover.cs
+++ Astro-Cat/Assets/Scenes/Default Planet/SceneMover.cs
@@ -2,12 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneMover : MonoBehaviour
 
 {
     public Camera firstPersonCamera;
     public Camera WormholeCamera;
+
+    //public Image Panel;
+    //float time = 0f;
+    //float F_time = 1f;
+
+    //public void Fade()
+    //{
+    //    StartCoroutine(FadeFlow());
+    //}
+    //IEnumerator FadeFlow()
+    //{
+    //    Panel.gameObject.SetActive(true);
+    //    Color alpha = Panel.color;
+    //    while (alpha.a < 1f)
+    //    {
+    //        time += Time.deltaTime / F_time;
+    //        alpha.a = Mathf.Lerp(0, 1, time);
+    //        Panel.color = alpha;
+    //        yield return null;
+    //    }
+    //    yield return null;
+    //}
 
     void Update()
 
@@ -19,6 +42,8 @@ public class SceneMover : MonoBehaviour
             //SceneManager.LoadScene("Wormhole");
             showWormHole();
             Invoke("sceneMove", 2.8f);
+            
+            //Fade();
         }
 
     }
@@ -32,6 +57,7 @@ public class SceneMover : MonoBehaviour
     public void sceneMove()
     {
         SceneManager.LoadScene("1st Planet");
+        //Fade();
     }
 
 
