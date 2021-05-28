27e92ce90c173fe1c6ca53ae095ebc09549eeac9
yangyujin <asxpyn@naver.com>
2021-05-28 15:07:08 +0900

    Add Scene transition, rigidbody on floor

---
+++ Astro-Cat/Assets/Scenes/Default Planet/SceneMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMover : MonoBehaviour
+
+{
+
+    void Update()
+
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player.transform.position.y <= -3f)
+
+        {
+
+            SceneManager.LoadScene("1st Planet");
+
+        }
+
+    }
+
+}
