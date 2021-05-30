70615f6ce86be28bb47a0575888db9b88227bf77
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-05-30 21:45:01 +0900

    Set Ghost Hunter Game

---
+++ Astro-Cat/Assets/Scenes/2nd Planet/MovingCat2ndGame.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingCat2ndGame : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool jDown;
+
+    bool isJump = false;
+
+    Vector3 moveVec;
+
+    Rigidbody rigid;
+    Animator anim;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+   
+        if (transform.position.y <= -3f) {
+            transform.position = new Vector3(9f, 1f, -15.1f);
+        }
+
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        jDown = Input.GetButton("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            anim.SetTrigger("doJump");
+            isJump = true;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+
+            isJump = false;
+
+        }
+    }
+}
