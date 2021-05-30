312b1562358c7ac56f310e6c46a0dd64658d65b0
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-05-30 19:52:57 +0900

    Fix some error on 1st planet

--- Astro-Cat/Assets/Scenes/1st Planet/PlayerMovement1st.cs
+++ Astro-Cat/Assets/Scenes/1st Planet/PlayerMovement1st.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement1st : MonoBehaviour
 {
@@ -15,11 +13,6 @@ public class PlayerMovement1st : MonoBehaviour
 
     Vector3 moveVec;
     Animator anim;
-
-    public GameObject talkPanel;
-    public Text text;
-    bool mouseClick = false;
-    int clickCount = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -35,38 +28,17 @@ public class PlayerMovement1st : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (clickCount == 0)
-            {
-                text.text = "보석은 잘 모아두면 분명 쓸모가 있을 것이다.\n이 은하계를 탈출하고 싶다면 보석을 꼭 기억해!";
-                clickCount++;
-            }
+        
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
 
-            else
-            {
-            
-                text.text = "이제 너가 이 행성에서 처음 도착했던 곳으로 다시 돌아가.\n새로운 모험이 널 기다리고 있을테니!";
-                talkPanel.SetActive(false);
-                SceneManager.LoadScene("Wormhole");
-            }
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        }
+        transform.position += moveVec * speed * Time.deltaTime;
 
-        if (mouseClick)
-        {
-            hAxis = Input.GetAxisRaw("Horizontal");
-            vAxis = Input.GetAxisRaw("Vertical");
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
 
-            moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-
-            transform.position += moveVec * speed * Time.deltaTime;
-
-            anim.SetBool("isWalk", moveVec != Vector3.zero);
-
-            transform.LookAt(transform.position + moveVec);
-        }
-           
+        transform.LookAt(transform.position + moveVec);
     }
 
     private void OnCollisionEnter(Collision collision)
