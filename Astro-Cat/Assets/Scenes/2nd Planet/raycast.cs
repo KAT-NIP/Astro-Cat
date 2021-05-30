5b9e78150a14862bec25710a128e2ed4915bb3e5
yangyujin <asxpyn@naver.com>
2021-05-30 19:58:28 +0900

    talkPanel..

--- Astro-Cat/Assets/Scenes/2nd Planet/raycast.cs
+++ Astro-Cat/Assets/Scenes/2nd Planet/raycast.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class raycast : MonoBehaviour
 {
+
+    bool mouseClick = false;
+    int clickCount = 0;
+
+    public GameObject talkPanel;
+    private Text talkObjectText;
+    public GameObject nametag;
+    private Text npcName;
+
+    private void Awake()
+    {
+        talkObjectText = GameObject.Find("talkPanel/Text").GetComponent<Text>();
+        npcName = GameObject.Find("talkPanel/nameTag/npcName").GetComponent<Text>();
+    }
 
     void Update()
     {
@@ -15,7 +30,18 @@ public class raycast : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.transform.gameObject);
+
+                if (hit.transform.gameObject.tag == "Devil")
+                {
+                    Debug.Log("what");
+                    GameObject.Find("Canvas").transform.Find("talkPanel").gameObject.SetActive(true);
+                    nametag.SetActive(true);
+                    npcName.text = "중독된 주민";
+                    talkObjectText.text = "콰아아아악 꺼져";
+
+                }
             }
+
         }
     }
 }
