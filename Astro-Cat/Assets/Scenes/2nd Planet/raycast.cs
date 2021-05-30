287455759542ab18029d62a1d48ef968b20096c4
yangyujin <asxpyn@naver.com>
2021-05-30 19:24:45 +0900

    Added raycast script on 2nd Planet

---
+++ Astro-Cat/Assets/Scenes/2nd Planet/raycast.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject);
+            }
+        }
+    }
+}
