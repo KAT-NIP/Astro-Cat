9e70b7de9f45b4b1d14abd4c60303336cd77df3c
yangyujin <asxpyn@naver.com>
2021-06-01 21:17:15 +0900

    Added script 2

--- Astro-Cat/Assets/Scenes/1 1st Planet/1st Planet/Script/PlayerScript1st.cs
+++ Astro-Cat/Assets/Scenes/1 1st Planet/1st Planet/Script/PlayerScript1st.cs
@@ -9,29 +9,39 @@ public class PlayerScript1st : MonoBehaviour
     public GameObject talkPanel;
     public Text text;
 
-    int clickCount;
+    int clickCount = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         clickCount = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log(clickCount);
+            
             if (clickCount == 0)
             {
                 text.text = "보석은 잘 모아두면 분명 쓸모가 있을 것이다.\n이 은하계를 탈출하고 싶다면 보석을 꼭 기억해!";
                 clickCount++;
             }
 
-            else
+            else if (clickCount == 1)
             {
-                text.text = "이제 너가 이 행성에서 처음 도착했던 곳으로 다시 돌아가.\n새로운 모험이 널 기다리고 있을테니!";
                 talkPanel.SetActive(false);
+                GameObject.Find("Gem").transform.Find("Diamond").gameObject.SetActive(true);
+                clickCount++;
+                
+            }
+
+
+            else if (clickCount == 2)
+            {
+                talkPanel.SetActive(true);
+                text.text = "이제 너가 이 행성에서 처음 도착했던 곳으로 다시 돌아가.\n새로운 모험이 널 기다리고 있을테니!";
+
             }
         }
     }
