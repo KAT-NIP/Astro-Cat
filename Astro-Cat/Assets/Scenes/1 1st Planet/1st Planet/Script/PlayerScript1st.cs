dc12df0f4ec8714ef993812d26f16bbf1967023e
yangyujin <asxpyn@naver.com>
2021-06-01 22:13:12 +0900

    Gem in 1st Planet after game finished

--- Astro-Cat/Assets/Scenes/1 1st Planet/1st Planet/Script/PlayerScript1st.cs
+++ Astro-Cat/Assets/Scenes/1 1st Planet/1st Planet/Script/PlayerScript1st.cs
@@ -11,6 +11,7 @@ public class PlayerScript1st : MonoBehaviour
     GameObject gem; // 보석
     int clickCount = 0;
     bool getGem = false;
+    bool lastClick = false;
 
     void Start()
     {
@@ -19,47 +20,49 @@ public class PlayerScript1st : MonoBehaviour
 
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
 
-            if (getGem == false && clickCount == 0)
+            if (!getGem && clickCount == 0)
             {
                 text.text = "보석은 잘 모아두면 분명 쓸모가 있을 것이다.\n이 은하계를 탈출하고 싶다면 보석을 꼭 기억해!";
-                clickCount++;
+                clickCount++; // 1
             }
 
-            else if (getGem == false && clickCount == 1) // 보석 나타남
+            else if (!getGem && clickCount == 1) // 보석 나타남
             {
                 talkPanel.SetActive(false);
                 GameObject.Find("Gem").transform.Find("Diamond").gameObject.SetActive(true);
-                clickCount++;
+                clickCount++; // 2
             }
 
-            else if (getGem == true && clickCount == 2)
-            {
+            else if (lastClick && clickCount == 2) // 마지막 말풍선 비활성화
+            { 
                 talkPanel.SetActive(false);
             }
 
+            Debug.Log(getGem);
             Debug.Log(clickCount);
         }
 
-        if (getGem == true) // 보석을 얻으면 말풍선이 바로 나타남
+        if (getGem && !lastClick) // 보석을 얻으면 말풍선이 바로 나타남
         {
             talkPanel.SetActive(true);
             text.text = "이제 너가 이 행성에서 처음 도착했던 곳으로 다시 돌아가.\n새로운 모험이 널 기다리고 있을테니!";
+            lastClick = true;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (getGem == true)
+
+        if (getGem && collision.gameObject.tag == "Floor")
         {
-            if (collision.gameObject.tag == "Floor")
-            {
-                Debug.Log("Stepped!");
-                SceneManager.LoadScene("Wormhole");
-            }
+            Debug.Log("Stepped!");
+            SceneManager.LoadScene("Wormhole");
         }
+
 
     }
 
@@ -67,9 +70,9 @@ public class PlayerScript1st : MonoBehaviour
     {
         if (other.tag == "Gem")
         {
-            getGem = true;
             gem = other.GetComponent<GameObject>();
             Destroy(other.gameObject);
+            getGem = true;
         }
     }
 }
