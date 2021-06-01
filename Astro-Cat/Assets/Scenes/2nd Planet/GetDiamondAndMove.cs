14aec6d679d5b635d9bddeb70dc0ae59c39a5cb7
n-y-kim <nyn2265@gmail.com>
2021-06-01 21:44:02 +0900

    change build setting + wormhole scene

--- Astro-Cat/Assets/Scenes/2nd Planet/GetDiamondAndMove.cs
+++ Astro-Cat/Assets/Scenes/2nd Planet/GetDiamondAndMove.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GetDiamondAndMove : MonoBehaviour
 {
+    public GameObject talkPanel;
+    public Text text;
+    public GameObject diamond;
+    bool mouseClick = false;
+    int clickCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,26 @@ public class GetDiamondAndMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (clickCount == 0)
+            {
+                talkPanel.SetActive(false);
+                clickCount++;
+                //보석 나타나게
+            }
+
+            else if(clickCount == 1)
+            {
+                talkPanel.SetActive(true);
+                text.text = "안녕히 가세요!!\n다음 행성은 까마귀를 조심하세요!";
+                clickCount++;
+            }
+
+            else if(clickCount == 2)
+            {
+                SceneManager.LoadScene("Wormhole");
+            }
+        }
     }
 }
