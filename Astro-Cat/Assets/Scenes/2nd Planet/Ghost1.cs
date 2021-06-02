24ebcffa1adfe9d31c188b8ab3f2735bc798ec44
yangyujin <asxpyn@naver.com>
2021-06-02 23:07:47 +0900

    Added Ghost Spawner

---
+++ Astro-Cat/Assets/Scenes/2nd Planet/Ghost1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost1 : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody Ghost1Rigidbody;
+
+    void Start()
+    {
+        Ghost1Rigidbody = GetComponent<Rigidbody>();
+        Ghost1Rigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 2f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject player = other.GetComponent<GameObject>();
+            Debug.Log("부딪힘");
+
+        }
+
+    }
+}
---
+++ Astro-Cat/Assets/Scenes/2nd Planet/GhostSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawner : MonoBehaviour
+{
+    public GameObject Ghost1Prefab;
+    public float spawnRateMin = 5f;
+    public float spawnRateMax = 5f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject Ghost1 = Instantiate(Ghost1Prefab, transform.position, transform.rotation);
+
+            Ghost1.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
