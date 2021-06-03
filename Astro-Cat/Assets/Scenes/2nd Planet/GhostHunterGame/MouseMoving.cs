1640c5ff2154963378764bbe43d67b6128a602ab
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-06-03 18:57:21 +0900

    Update....
    아직덜됏어요..

---
+++ Astro-Cat/Assets/Scenes/2nd Planet/GhostHunterGame/MouseMoving.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMoving : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool jDown;
+
+    bool isJump = false;
+
+    Vector3 moveVec;
+
+    Rigidbody rigid;
+    Animator anim;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+
+        if (transform.position.y <= -3f)
+        {
+            transform.position = new Vector3(9f, 1f, -15.1f);
+        }
+
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        jDown = Input.GetButton("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+    }
+
+    void Turn()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = Camera.main.farClipPlane;
+
+        Vector3 newPerspective = Camera.main.ScreenToWorldPoint(pos);
+        newPerspective.y = 0;
+        transform.LookAt(transform.position + newPerspective.normalized);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            anim.SetTrigger("doJump");
+            isJump = true;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+        }
+    }
+}
