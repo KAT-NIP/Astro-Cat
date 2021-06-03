c514f54b5e73aba1325c9cba2b75a436643c30c5
yangyujin <asxpyn@naver.com>
2021-06-03 18:40:56 +0900

    Added Ghost3

---
+++ Astro-Cat/Assets/Scenes/2nd Planet/Ghost3.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost3 : MonoBehaviour
+{
+    //public float speed = 8f;
+    //private Rigidbody Ghost1Rigidbody;
+    NavMeshAgent nav;
+    GameObject target;
+
+
+    void Start()
+    {
+        //Ghost1Rigidbody = GetComponent<Rigidbody>();
+        //Ghost1Rigidbody.velocity = transform.forward * speed;
+
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        //Destroy(gameObject, 2f);
+    }
+
+    void Update()
+    {
+        // 유령이 플레이어를 따라오게
+        //if (nav.destination != target.transform.position)
+        //{
+        nav.SetDestination(target.transform.position);
+        //}
+        //else
+        //{
+        //    nav.SetDestination(transform.position);
+        //}
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject player = other.GetComponent<GameObject>();
+            Debug.Log("부딪힘");
+
+        }
+
+    }
+}
--- Astro-Cat/Assets/Scenes/2nd Planet/GhostSpawner.cs
+++ Astro-Cat/Assets/Scenes/2nd Planet/GhostSpawner.cs
@@ -6,6 +6,7 @@ public class GhostSpawner : MonoBehaviour
 {
     public GameObject Ghost1Prefab;
     public GameObject Ghost2Prefab;
+    public GameObject Ghost3Prefab;
     public float spawnRateMin = 7f;
     public float spawnRateMax = 7f;
 
@@ -33,6 +34,7 @@ public class GhostSpawner : MonoBehaviour
 
             GameObject Ghost1 = Instantiate(Ghost1Prefab, transform.position, transform.rotation);
             GameObject Ghost2 = Instantiate(Ghost2Prefab, transform.position, transform.rotation);
+            GameObject Ghost3 = Instantiate(Ghost3Prefab, transform.position, transform.rotation);
             //Ghost1.transform.LookAt(target);
 
             spawnRate = Random.Range(spawnRateMin, spawnRateMax);
