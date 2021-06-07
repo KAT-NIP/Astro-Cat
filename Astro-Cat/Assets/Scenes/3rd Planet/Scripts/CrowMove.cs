3ebec21f63865cf72658f0561251935f54d2ea07
n-y-kim <nyn2265@gmail.com>
2021-06-07 20:49:41 +0900

    add crow

---
+++ Astro-Cat/Assets/Scenes/3rd Planet/Scripts/CrowMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CrowMove : MonoBehaviour
+{
+    NavMeshAgent nav;
+    GameObject target;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(target.transform.position);
+    }
+}
