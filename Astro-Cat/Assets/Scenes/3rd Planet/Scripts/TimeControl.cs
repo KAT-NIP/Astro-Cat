4431d58c5e83a9c2c99d7fe64ea8e0b744ab3c94
n-y-kim <nyn2265@gmail.com>
2021-06-09 20:08:46 +0900

    timer complete

--- Astro-Cat/Assets/Scenes/3rd Planet/Scripts/TimeControl.cs
+++ Astro-Cat/Assets/Scenes/3rd Planet/Scripts/TimeControl.cs
@@ -9,36 +9,62 @@ public class TimeControl : MonoBehaviour
     public Text[] timeText;
     public Text gameOverText;
     float time;
+    //int min = 0;
     // Start is called before the first frame update
     void Start()
     {
         //제한 시간 2
         timeText[0].text = "02";
         timeText[1].text = "00";
+
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        timeText[0].text = "0" + (1 - ((int)time / 60 % 60)).ToString();
-        if(60 - ((int)time % 60) == 60)
+        int min = (int)time / 60 % 60;
+        int sec = (int)time % 60;
+        
+        Debug.Log("min = " + min);
+        Debug.Log("sec = " + sec);
+
+        if (min == 0 && sec == 0)
         {
-            timeText[1].text = "00";
+            timeText[0].text = "02";
+            //min += 1;
         }
-        else if(60 - ((int)time % 60) < 10)
+        else
         {
-            timeText[1].text = "0" + (60 - ((int)time % 60)).ToString();
-        } else
-        {
-            timeText[1].text = (60 - ((int)time % 60)).ToString();
+            timeText[0].text = "0" + (1 - min).ToString();
         }
 
-        if(1 - ((int)time / 60 % 60) == 0 && (int)time % 60 == 0)
+
+        if (60 - sec == 60)
         {
+            if (1 - min == 0)
+            {
+                timeText[0].text = "01";
+            }
+            timeText[1].text = "00";
+        }
+
+        else if(60 - sec < 10)
+        {
+            timeText[1].text = "0" + (60 - sec).ToString();
+        }
+
+        else
+        {
+            timeText[1].text = (60 - sec).ToString();
+        }
+
+        if (1 - min == 0 && 60 - sec == 0)
+        {
+            Debug.Log((1 - min) + (60 - sec));
             gameOverText.enabled = true;
             Invoke("restartGame", 5f);
-            
+
         }
     }
 
