f3e10f3335a6938cda62901a3f55ae4e1c976c07
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-06-12 03:05:29 +0900

    1차

---
+++ Astro-Cat/Assets/Scenes/Wormhole_FX/Scripts/WormholeIntroTo1st.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WormholeIntroTo1st : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -40)
+        {
+            SceneManager.LoadScene("Default Planet");
+        }
+    }
+}
